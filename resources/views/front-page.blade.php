f588d443100c2306811a85925779223ba06eb1f1
alex-418 <aparedes@bccampus.ca>
2019-01-30 13:53:38 -0800

    add customizer option, func to return hero page, frontpage blade structure

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,8 +1,20 @@
 @extends('layouts.front')
 
 @section('content')
-	<span itemscope itemtype="http://schema.org/ItemPage">
-		<div class="row mt-sm-4">
+    <span itemscope itemtype="http://schema.org/ItemPage">
+
+          <div class="row">
+    <div class="col-md-6">
+     	<h2>{!! $get_hero_page->post_title !!}</h2>
+        <p>{!! $get_hero_page->post_content !!}</p>
+        <a class="btn btn-primary" href="{!! get_permalink($get_hero_page->ID) !!}" role="button">Learn More</a>
+    </div>
+    <div class="col-md-6">
+		{!! get_the_post_thumbnail($get_hero_page->ID) !!}
+    </div>
+  </div>
+
+        <div class="row mt-sm-4">
 	  <div class="col-md-4">
 PLACEHOLDER
 	  </div>
