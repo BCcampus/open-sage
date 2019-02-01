97d457383810c5096748184732b702460d0b8747
alex-418 <aparedes@bccampus.ca>
2019-01-31 16:08:04 -0800

    match design styling, center card headings

--- resources/views/partials/front-page-cards.blade.php
+++ resources/views/partials/front-page-cards.blade.php
@@ -2,12 +2,12 @@
         <article class="homepage-cards-item col-md-6 mb-2" itemscope itemtype="http://schema.org/Article">
             <a href="{{ get_permalink($get_left_card_id) }}" class="img-link">
                 <div class="featured-topic row-fluid d-flex" style="background-image: url({!! get_the_post_thumbnail_url($get_left_card_id) !!});">
-                    <h4 itemprop="name" class="blue-bkgd text-inverse col-sm mt-auto">{{get_the_title($get_left_card_id)}}
+                    <h4 itemprop="name" class="blue-bkgd text-inverse col-sm mt-auto text-center">{{get_the_title($get_left_card_id)}}
                     </h4>
                 </div>
             </a>
             <div class="row-fluid min-height-md">
-                <p class="pt-3 pad-left pad-right">
+                <p class="p-3">
                     @if (has_excerpt($get_left_card_id))
                         {{ get_the_excerpt($get_left_card_id) }}
                     @else
@@ -15,7 +15,7 @@
                     @endif
                 </p>
                 <div class="col text-center">
-                    <a class="btn btn-primary" href="{{ get_permalink($get_left_card_id) }}" role="button">Learn More</a>
+                    <a class="btn btn-primary mb-3" href="{{ get_permalink($get_left_card_id) }}" role="button">Learn More</a>
                 </div>
             </div>
             <meta itemprop="author" content="{{get_post_field('post_author', $get_left_card_id)}}"/>
@@ -33,12 +33,12 @@
     <article class="homepage-cards-item col-md-6 mb-2" itemscope itemtype="http://schema.org/Article">
         <a href="{{ get_permalink($get_right_card_id) }}" class="img-link">
             <div class="featured-topic row-fluid d-flex" style="background-image: url({!! get_the_post_thumbnail_url($get_right_card_id) !!});">
-                <h4 itemprop="name" class="blue-bkgd text-inverse col-sm mt-auto">{{get_the_title($get_right_card_id)}}
+                <h4 itemprop="name" class="blue-bkgd text-inverse col-sm mt-auto text-center">{{get_the_title($get_right_card_id)}}
                 </h4>
             </div>
         </a>
         <div class="row-fluid min-height-md">
-            <p class="pt-3 pad-left pad-right">
+            <p class="p-3">
                 @if (has_excerpt($get_right_card_id))
                     {{ get_the_excerpt($get_right_card_id) }}
                 @else
@@ -46,7 +46,7 @@
                 @endif
             </p>
             <div class="col text-center">
-                <a class="btn btn-primary" href="{{ get_permalink($get_right_card_id) }}" role="button">Learn More</a>
+                <a class="btn btn-primary mb-3" href="{{ get_permalink($get_right_card_id) }}" role="button">Learn More</a>
             </div>
         </div>
         <meta itemprop="author" content="{{get_post_field('post_author', $get_right_card_id)}}"/>
