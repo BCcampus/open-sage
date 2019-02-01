4e5bdc6a090785502ae86afde424a3d641545879
bdolor <brad@bradpayne.ca>
2019-02-01 12:47:56 -0800

    remove commented out code

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,20 +1,12 @@
 @extends('layouts.front')
 
 @section('content')
-	<span itemscope itemtype="http://schema.org/ItemPage">
+    <span itemscope itemtype="http://schema.org/ItemPage">
 		<div class="row">
-			{{--@include('partials.front-page-hero')--}}
+			@include('partials.front-page-hero')
 		</div>
 		<div class="row">
 			@include('partials.search-collection')
 		</div>
-		<div class="row mt-sm-4">
-	  	<div class="col-md-6">
-			<h2>PLACEHOLDER</h2>
-	  	</div>
-	  	<div class="col-md-6">
-			<h2>PLACEHOLDER</h2>
-	  	</div>
-	  	</div>
   	</span>
 @endsection
