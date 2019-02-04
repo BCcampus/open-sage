6d9d0ba4739795a3cb0a67e21c36c619e8bc171f
bdolor <brad@bradpayne.ca>
2019-02-04 13:36:30 -0800

    stats section front end only

---
+++ resources/views/partials/stats-collection.blade.php
@@ -0,0 +1,26 @@
+<section class="bkgd-blue-navy d-flex flex-row flex-wrap full-width">
+    <div class="col-12 py-3">
+        <h4 class="text-center text-inverse text-white">Open Education at BCcampus</h4>
+        <p class="text-center text-white">The high cost of educational resources and textbooks creates a serious obstacle to the access of post-secondary education</p>
+    </div>
+    <span class="col-md-3 text-center text-white"><a href="#" class="stats-icons">
+            <img src="@asset('images/statsicon-institution.svg')" alt="icon representing an institution"></a>
+        <p>PLACEHOLDER</p>
+        <p>Institutions</p>
+    </span>
+    <span class="col-md-3 text-center text-white"><a href="#" class="stats-icons">
+            <img src="@asset('images/statsicon-faculty.svg')" alt="icon representing faculty"></a>
+        <p>PLACEHOLDER</p>
+        <p>Faculty</p>
+    </span>
+    <span class="col-md-3 text-center text-white"><a href="#" class="stats-icons">
+            <img src="@asset('images/statsicon-students.svg')" alt="icon representing students"></a>
+        <p>PLACEHOLDER</p>
+        <p>Students</p>
+    </span>
+    <span class="col-md-3 text-center text-white"><a href="#" class="stats-icons">
+            <img src="@asset('images/statsicon-savings.svg')" alt="icon representing savings"></a>
+        <p>$ PLACEHOLDER</p>
+        <p>Savings</p>
+    </span>
+</section>
