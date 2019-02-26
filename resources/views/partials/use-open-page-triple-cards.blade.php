62eea2c10500fbff928323c3a5a27756b3e450f7
alex-418 <aparedes@bccampus.ca>
2019-02-25 16:08:50 -0800

    add lorem ipsum placehoder text

--- resources/views/partials/use-open-page-triple-cards.blade.php
+++ resources/views/partials/use-open-page-triple-cards.blade.php
@@ -1,3 +1,5 @@
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
 <section class="homepage-cards d-flex flex-row flex-wrap full-width">
         <article class="homepage-cards-item col-md-4 mb-2" itemscope itemtype="http://schema.org/Article">
             <a href="{{ get_permalink($get_use_open_triple_left_id) }}" class="img-link">
