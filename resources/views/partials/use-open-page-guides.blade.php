093a5ca03ad543f5e2618e90e3c5b9df8d955867
alex-418 <aparedes@bccampus.ca>
2019-03-01 10:36:32 -0800

    fix adaptation guide typo

--- resources/views/partials/use-open-page-guides.blade.php
+++ resources/views/partials/use-open-page-guides.blade.php
@@ -5,7 +5,7 @@
             <p class="card-text">Refer to these useful guides to learn more about self-publishing, adopting, and adapting open textbooks and other OER.</p>
             <ul>
                 <li><a href="https://opentextbc.ca/selfpublishguide/">Self-Publishing Guide</a></li>
-                <li><a href="https://opentextbc.ca/adaptopentextbook/">Adaption Guide</a></li>
+                <li><a href="https://opentextbc.ca/adaptopentextbook/">Adaptation Guide</a></li>
                 <li><a href="https://opentextbc.ca/adoptopentextbook/">Adoption Guide</a></li>
             </ul>
         </div>
