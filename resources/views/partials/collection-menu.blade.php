06a658b74797527a13de5d92516e51cce0072bb2
bdolor <brad@bradpayne.ca>
2019-03-06 12:39:53 -0800

    merge dev

--- resources/views/partials/collection-menu.blade.php
+++ resources/views/partials/collection-menu.blade.php
@@ -1,5 +1,26 @@
 @php
-    if ( file_exists( $menu = ABSPATH . 'wp-content/opentextbooks/assets/templates/partial/menu.php' ) ) {
-    require_once $menu;
-    }
+    $alpha = true;
+    $summary = false;
+    $subject_areas = \App\Controllers\Page::getSubjectStats($summary,$alpha)
 @endphp
+<div class="tabs-left accordion" id="accordion2">
+    <ul class="accordion-group list-unstyled">
+        <div class="accordion-group">
+            <li class="accordion-heading"><a class="accordion-toggle" href="?subject=">All Subjects</a></li>
+        </div>
+        @foreach($subject_areas as $sub1=>$sub2)
+            <div class="accordion-group">
+                @php($abr=substr($sub1,0,4))
+                <li class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse"
+                                                 data-parent="#accordion2" href="#collapse{{$abr}}">{{$sub1}}</a></li>
+                <div id="collapse{{$abr}}" class="accordion-body collapse">
+                    <ul>
+                        @foreach($sub2 as $k=>$num)
+                            <li><a href="?{{$k}}">{{$k}}</a> ({{$num}})</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        @endforeach
+    </ul>
+</div>
