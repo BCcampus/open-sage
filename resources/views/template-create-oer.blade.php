4b1bd56918a9757cc00e9bc0cd735bdc19ecc9af
bdolor <brad@bradpayne.ca>
2019-03-12 10:36:04 -0700

    add create template and add component

---
+++ resources/views/template-create-oer.blade.php
@@ -0,0 +1,14 @@
+{{--
+  Template Name: Create OER
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+    @include('partials.page-header')
+
+    <div class="row">
+        @include('partials.collection-guides-toolkits')
+    </div>
+
+@endsection
