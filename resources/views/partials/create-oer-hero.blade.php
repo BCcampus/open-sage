81eacb443f4ae6face6f19152b621eb613635d0b
alex-418 <aparedes@bccampus.ca>
2019-03-12 16:09:56 -0700

    a little padding for the heros

--- resources/views/partials/create-oer-hero.blade.php
+++ resources/views/partials/create-oer-hero.blade.php
@@ -1,5 +1,5 @@
 <section class="d-flex flex-row flex-wrap full-width">
-<div class="col-md-6 bkgd-blue-navy">
+<div class="col-md-6 bkgd-blue-navy p-3">
     <h2>{!! get_the_title($get_hero_create_oer) !!}</h2>
     <p class="py-3">{{\App\Controllers\App::getPostExcerpt($get_hero_create_oer)}}</p>
     <a class="btn btn-primary mb-3" href="{{\App\Controllers\App::maybeGuid($get_hero_create_oer, get_post_field('post_name', $get_hero_create_oer))}}" role="button">Learn More</a>
--- resources/views/partials/front-page-hero.blade.php
+++ resources/views/partials/front-page-hero.blade.php
@@ -1,5 +1,5 @@
 <section class="d-flex flex-row flex-wrap full-width">
-<div class="col-md-6 bkgd-blue-navy">
+<div class="col-md-6 bkgd-blue-navy p-3">
     <h2>{!! get_the_title($get_hero_page_id) !!}</h2>
     <p class="py-3">{{\App\Controllers\App::getPostExcerpt($get_hero_page_id)}}</p>
     <a class="btn btn-primary mb-3" href="{{\App\Controllers\App::maybeGuid($get_hero_page_id, get_post_field('post_name', $get_hero_page_id))}}" role="button">Learn More</a>
