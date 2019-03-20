207ba2a6fecc9a0233337a1ef0398dd82d545d19
alex-418 <aparedes@bccampus.ca>
2019-03-20 00:31:00 -0700

    Update guides and toolkits heading text

--- resources/views/partials/use-open-page-guides.blade.php
+++ resources/views/partials/use-open-page-guides.blade.php
@@ -1,7 +1,7 @@
 <section>
     <div class="card m-3">
         <div class="card-body">
-            <h5 class="card-title">Faculty open educational resources (OER) toolkits</h5>
+            <h5 class="card-title">Open educational resources (OER) toolkits</h5>
             <p class="card-text">Refer to these useful guides to learn more about self-publishing, adopting, and adapting open textbooks and other OER.</p>
             <ul>
                 <li><a href="https://opentextbc.ca/selfpublishguide/">Self-Publishing Guide</a></li>
