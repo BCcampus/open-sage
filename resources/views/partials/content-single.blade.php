6594b59963f320451a91d86fd045ecb0fabc8fcd
alex-418 <aparedes@bccampus.ca>
2019-03-21 13:35:09 -0700

    Fixes issues with HTML from the content editor not displaying properly

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,8 +1,10 @@
 <article @php post_class() @endphp>
+<!--
   <header>
     <h1 class="entry-title">{{ get_the_title() }}</h1>
     @include('partials/entry-meta')
   </header>
+  -->
   <div class="entry-content">
     @php the_content() @endphp
   </div>
--- resources/views/template-what-is-open.blade.php
+++ resources/views/template-what-is-open.blade.php
@@ -10,9 +10,8 @@
         @include('partials.what-is-open-card')
     </div>
     <div class="row">
-        @while(have_posts())
-            {!! the_post() !!}
-            @include('partials/content-page')
+        @while(have_posts()) @php the_post() @endphp
+        @include('partials.content-single-'.get_post_type())
         @endwhile
     </div>
 @endsection
