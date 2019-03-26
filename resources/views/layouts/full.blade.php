40adf9ba6a55f99f5f24901140f25d05672d74c2
alex-418 <aparedes@bccampus.ca>
2019-03-26 10:51:24 -0700

    moves breadcrumbs to partial, fixes position for default pages

--- resources/views/layouts/full.blade.php
+++ resources/views/layouts/full.blade.php
@@ -7,6 +7,7 @@
 @php(do_action('get_header'))
 @include('partials.header')
 <div class="wrap container-fluid" role="document">
+    @include('partials.breadcrumbs')
     <main class="main">
         @yield('content')
     </main>
--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,21 +1,3 @@
-@if( (! is_front_page()) && ! is_page(['subscribe', 'subscribe-confirm']))
-	<nav class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
-		@foreach ( $bread_crumbs as $key => $item )
-			<span class="breadcrumb-item" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-				@if ( empty( $item['link'] ) )
-					<span itemscope itemtype="http://schema.org/Thing" itemprop="item">
-						<span itemprop="name">{{ esc_html( $item['title'] ) }}</span>
-					</span>
-				@else
-					<a itemscope itemtype="http://schema.org/Thing" itemprop="item"
-					   href="{{ esc_url( $item['link'] ) }}"><span
-								itemprop="name">{{ esc_html( $item['title'] ) }}</span></a>
-				@endif
-		  <meta itemprop="position" content="<?php echo esc_attr( $key + 1 ); ?>"/>
-				</span>
-		@endforeach
-	</nav>
-@endif
 @include('partials.collection-menu-mobile')
 <div class="page-header">
 	<h1>{!! \App\Controllers\App::title() !!}</h1>
