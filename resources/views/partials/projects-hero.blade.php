89c3a1ca4d38a0c8265198db00819f5c5e103d8a
alex-418 <aparedes@bccampus.ca>
2019-03-26 13:13:16 -0700

    add template and hero partial for grants and projects

---
+++ resources/views/partials/projects-hero.blade.php
@@ -0,0 +1,11 @@
+<section class="d-flex flex-row flex-wrap full-width">
+<div class="col-md-6 bkgd-blue-navy p-3">
+    <h2>{!! get_the_title($get_hero_projects) !!}</h2>
+    <p class="py-3">{{\App\Controllers\App::getPostExcerpt($get_hero_projects)}}</p>
+    <a class="btn btn-primary mb-3" href="{{\App\Controllers\App::maybeGuid($get_hero_projects, get_post_field('post_name', $get_hero_projects))}}" role="button">Learn More</a>
+</div>
+<div class="col-md-6 p-0">
+    <div class="featured-topic row-fluid d-flex" style="background-image: url({{\App\Controllers\App::getThumbUrl($get_hero_projects)}});">
+    </div>
+</div>
+</section>
