4d5c1a746239a929ea68ed584e37cb0c0b6eca33
alex-418 <aparedes@bccampus.ca>
2019-04-03 15:24:58 -0700

    decode html entities in the breadcrumbs

--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -6,12 +6,12 @@
 				<span class="breadcrumb-item" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
 				@if ( empty( $item['link'] ) )
 						<span itemscope itemtype="http://schema.org/Thing" itemprop="item">
-						<span itemprop="name">{{ esc_html( $item['title'] ) }}</span>
+						<span itemprop="name">{{ html_entity_decode( $item['title'] ) }}</span>
 					</span>
 					@else
 						<a itemscope itemtype="http://schema.org/Thing" itemprop="item"
 						   href="{{ esc_url( $item['link'] ) }}"><span
-									itemprop="name">{{ esc_html( $item['title'] ) }}</span></a>
+									itemprop="name">{{ html_entity_decode( $item['title'] ) }}</span></a>
 					@endif
 					<meta itemprop="position" content="<?php echo esc_attr( $key + 1 ); ?>"/>
 				</span>
