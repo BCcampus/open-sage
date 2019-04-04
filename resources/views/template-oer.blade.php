880dc2173ffb3e7fef07eb525ee4b2c8e0b1c134
alex-418 <aparedes@bccampus.ca>
2019-04-04 14:10:00 -0700

    completes #184

--- resources/views/template-oer.blade.php
+++ resources/views/template-oer.blade.php
@@ -29,14 +29,6 @@
         @include('partials.collection-guides-toolkits')
     </div>
     <div class="row">
-        <h3>B.C. open textbook MARC records</h3>
-        <p>Gain access to machine-readable cataloguing (MARC) records for all of the content created for BCcampus Open
-            Education.</p>
-        <h3>Other collections</h3>
-        <p>The B.C. Open Textbook Collection continues to expand, so we’ve compiled a list of open textbook libraries to
-            connect you with the open educational resources (OER) you may need for your institution.</p>
-    </div>
-    <div class="row">
         @include('partials.oer-cards')
     </div>
 @endsection
