1660e982fb7fd7d99976f7069a7f35739c1a7014
bdolor <brad@bradpayne.ca>
2019-04-04 14:47:53 -0700

    add missing closing tag

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.full')
 
 @section('content')
   @include('partials.page-header')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,27 +1,27 @@
 <!doctype html>
 <html {!! get_language_attributes() !!}>
-  @include('partials.head')
-  <body @php body_class() @endphp>
-  @include('partials.microdata-open')
-  @include('partials.uio')
-    @php do_action('get_header') @endphp
-    @include('partials.header')
-    <div class="wrap container-fluid" role="document">
-      @include('partials.breadcrumbs')
-      <div class="content row">
+@include('partials.head')
+<body @php body_class() @endphp>
+@include('partials.microdata-open')
+@include('partials.uio')
+@php do_action('get_header') @endphp
+@include('partials.header')
+<div class="wrap container-fluid" role="documents">
+    @include('partials.breadcrumbs')
+    <div class="content row">
         <aside id="sidebar" class="col-lg-3 mt-2">
-          @include('partials.child-menu-mobile')
-          @include('partials.sidebar')
+            @include('partials.child-menu-mobile')
+            @include('partials.sidebar')
         </aside>
         <main class="main col-lg-8">
-          @yield('content')
+            @yield('content')
         </main>
-      </div>
     </div>
-    @php do_action('get_footer') @endphp
-    @include('partials.footer')
-    @php wp_footer() @endphp
-  @include('partials.uio-script')
-  @include('partials.microdata-close')
-  </body>
+</div>
+@php do_action('get_footer') @endphp
+@include('partials.footer')
+@php wp_footer() @endphp
+@include('partials.uio-script')
+@include('partials.microdata-close')
+</body>
 </html>
