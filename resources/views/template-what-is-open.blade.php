f38de5115c8e049b01602aa110c6f513c8b18b90
bdolor <brad@bradpayne.ca>
2019-04-05 12:33:26 -0700

    remove unnecessary rows

--- resources/views/template-what-is-open.blade.php
+++ resources/views/template-what-is-open.blade.php
@@ -6,12 +6,8 @@
 
 @section('content')
     @include('partials.page-header')
-    <div class="row">
         @include('partials.what-is-open-card')
-    </div>
-    <div class="row">
         @while(have_posts()) @php the_post() @endphp
         @include('partials.content-single-'.get_post_type())
         @endwhile
-    </div>
 @endsection
