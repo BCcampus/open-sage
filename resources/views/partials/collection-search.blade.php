74a6b2de9d110a8066f678be493ffa78188266c2
bdolor <brad@bradpayne.ca>
2019-04-05 10:25:54 -0700

    correct text and bkgd same colour

--- resources/views/partials/collection-search.blade.php
+++ resources/views/partials/collection-search.blade.php
@@ -1,10 +1,10 @@
 @php
     $link = get_home_url() . '/find-open-textbooks';
     $nonce = wp_create_nonce('find-oer');
-    $bkgd = (is_page_template(['views/template-oer.blade.php', 'views/template-find-oer.blade.php'])) ? 'bkgd-blue-navy' : 'bkgd-grey-light';
+    $bkgd = (is_page_template(['views/template-oer.blade.php', 'views/template-find-oer.blade.php'])) ? 'blue-navy' : 'grey-light';
 @endphp
-<section class="{{$bkgd}} full-width py-4 px-5 mb-3">
-        <h2 class="text-center text-blue-navy">Search the B.C. Open Textbook Collection</h2>
+<section class="bkgd-{{$bkgd}} full-width py-4 px-5 mb-3">
+        <h2 class="text-center">Search the B.C. Open Textbook Collection</h2>
         <form class='form-group input-group mb-0' action='{{$link}}' method='get' role="search">
         <label for="find-oer-1" class="sr-only">Search the BC Open Textbook Collection</label>
         <input type='text' class='form-control' placeholder='Search...' name='search' id='find-oer-1' aria-label="Search open textbook collection" aria-describedby="find-oer-2"/>
