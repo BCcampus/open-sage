197bebaaf0e6ab6ed2d88b9009bec71205729051
bdolor <brad@bradpayne.ca>
2019-04-05 13:56:01 -0700

    fix projects template

--- resources/views/template-projects.blade.php
+++ resources/views/template-projects.blade.php
@@ -5,12 +5,8 @@
 @extends('layouts.app')
 @section('content')
     @include('partials.page-header')
-    <div class="row">
-        @include('partials.projects-hero')
-    </div>
-    <div class="row">
-        @while(have_posts()) @php the_post() @endphp
-        @include('partials.content-single-'.get_post_type())
-        @endwhile
-    </div>
+    @include('partials.projects-hero')
+    @while(have_posts()) @php the_post() @endphp
+    @include('partials.content-single-'.get_post_type())
+    @endwhile
 @endsection
