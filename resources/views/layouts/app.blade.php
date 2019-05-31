88a7d96d6a265af1c08c3fece2cd379a00eb93f7
alex-418 <aparedes@bccampus.ca>
2019-05-31 11:25:42 -0700

    remove display prefs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
 @include('partials.head')
 <body @php body_class() @endphp>
 @include('partials.microdata-open')
-@include('partials.uio')
 @php do_action('get_header') @endphp
 @include('partials.header')
 <div class="wrap container-fluid" role="document">
@@ -21,7 +20,6 @@
 @php do_action('get_footer') @endphp
 @include('partials.footer')
 @php wp_footer() @endphp
-@include('partials.uio-script')
 @include('partials.microdata-close')
 </body>
 </html>
